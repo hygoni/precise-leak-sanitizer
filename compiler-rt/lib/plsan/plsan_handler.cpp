95ca37d40c79594532f94c849baa6d535f69891d
purplepig4657 <purplepig4657@gmail.com>
2024-02-03 03:57:57 +0000

    Fix: remove backword.h include

--- compiler-rt/lib/plsan/plsan_handler.cpp
+++ compiler-rt/lib/plsan/plsan_handler.cpp
@@ -6,7 +6,7 @@ namespace __plsan {
 void PlsanHandler::exception_check(RefCountAnalysis ref_count_analysis) {
   AddrType addrType;
   ExceptionType exceptionType;
-  print_stack_trace(NULL, 0, std::cerr);
+  // print_stack_trace(NULL, 0, std::cerr);
   exit(EXIT_FAILURE);
 }
 
@@ -21,43 +21,45 @@ void PlsanHandler::exception_check(RefCountAnalysis ref_count_analysis) {
 //     #1 0x401150 in main /home/hyeyoo/precise-leak-sanitizer/main.c:7
 //     #2 0x7fd7dd83feaf in __libc_start_call_main (/lib64/libc.so.6+0x3feaf)
 
-void PlsanHandler::print_stack_trace(void *alloc_addr, size_t alloc_size,
-                                     std::ostream &os) {
-  os << "=================================================================\n";
+// void PlsanHandler::print_stack_trace(void *alloc_addr, size_t alloc_size,
+//                                      std::ostream &os) {
+//   os <<
+//   "=================================================================\n";
 
-  os << "==<" << getpid()
-     << ">==ERROR: PreciseLeakSanitizer: detected memory leaks\n";
+//   os << "==<" << getpid()
+//      << ">==ERROR: PreciseLeakSanitizer: detected memory leaks\n";
 
-  os << "\n";
-  os << "Leak of " << alloc_size << " byte(s) in an object (<" << alloc_addr
-     << ">) allocated from:\n";
-  stack_trace(alloc_addr, os);
+//   os << "\n";
+//   os << "Leak of " << alloc_size << " byte(s) in an object (<" << alloc_addr
+//      << ">) allocated from:\n";
+//   stack_trace(alloc_addr, os);
 
-  StackTrace st;
-  TraceResolver tr;
-  st.load_here();
-  tr.load_stacktrace(st);
+//   StackTrace st;
+//   TraceResolver tr;
+//   st.load_here();
+//   tr.load_stacktrace(st);
 
-  void *leak_addr = tr.resolve(st[0]).addr;
+//   void *leak_addr = tr.resolve(st[0]).addr;
 
-  os << "\n";
-  os << "Last reference to the object (<" << leak_addr << ">) lost at:\n";
-  stack_trace(leak_addr, os);
-}
+//   os << "\n";
+//   os << "Last reference to the object (<" << leak_addr << ">) lost at:\n";
+//   stack_trace(leak_addr, os);
+// }
 
-void PlsanHandler::stack_trace(void *addr, std::ostream &os) {
-  StackTrace st;
-  TraceResolver tr;
+// void PlsanHandler::stack_trace(void *addr, std::ostream &os) {
+//   StackTrace st;
+//   TraceResolver tr;
 
-  addr == NULL ? st.load_here() : st.load_from(addr);
-  tr.load_stacktrace(st);
+//   addr == NULL ? st.load_here() : st.load_from(addr);
+//   tr.load_stacktrace(st);
 
-  for (size_t i = 0; i < st.size(); ++i) {
-    ResolvedTrace trace = tr.resolve(st[i]);
+//   for (size_t i = 0; i < st.size(); ++i) {
+//     ResolvedTrace trace = tr.resolve(st[i]);
 
-    os << "    #" << i << " " << trace.addr << " in " << trace.object_function
-       << " " << trace.object_filename << ":" << trace.source.line << "\n";
-  }
-}
+//     os << "    #" << i << " " << trace.addr << " in " <<
+//     trace.object_function
+//        << " " << trace.object_filename << ":" << trace.source.line << "\n";
+//   }
+// }
 
 } // namespace __plsan
--- compiler-rt/lib/plsan/plsan_handler.h
+++ compiler-rt/lib/plsan/plsan_handler.h
@@ -2,10 +2,11 @@
 #define PLSAN_HANDLER_H
 
 #include "lsan/lsan_common.h"
+#include <stdint.h>
 
-#include "lib/backward.h"
+// #include "lib/backward.h"
 
-using namespace backward;
+// using namespace backward;
 
 namespace __plsan {
 
@@ -21,8 +22,8 @@ struct RefCountAnalysis {
 class PlsanHandler {
 public:
   void exception_check(RefCountAnalysis ref_count_analysis);
-  void print_stack_trace(void *alloc_addr, size_t alloc_size, std::ostream &os);
-  void stack_trace(void *addr, std::ostream &os);
+  // void print_stack_trace(void *alloc_addr, size_t alloc_size, std::ostream
+  // &os); void stack_trace(void *addr, std::ostream &os);
 };
 
 } // namespace __plsan
