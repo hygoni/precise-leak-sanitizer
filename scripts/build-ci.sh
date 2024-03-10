aaf5906fb0aa1d745e03d4f71c1bfa6cab63e6ea
Hyeonggon Yoo <42.hyeyoo@gmail.com>
2024-03-10 14:17:21 +0900

    workflow: run benchmarks automatically and flamegraph generation
    Measure how much a program becomes slower after instrumentation.
    Also, generate flamegraph automatically. Testcases are based on
    https://github.com/fragglet/c-algorithms .
    
    For CPU profiling, add COMPILER_RT_STACK_UNWIND option which adds
    -g and -fno-omit-frame-pointer options when building sanitizer runtime
    libraries.
    
    Signed-off-by: Hyeonggon Yoo <42.hyeyoo@gmail.com>

--- scripts/build-ci.sh
+++ scripts/build-ci.sh
@@ -7,6 +7,7 @@ cd build
 # 2. Build
 cmake -DLLVM_ENABLE_PROJECTS="llvm;clang;compiler-rt" \
         -DCMAKE_BUILD_TYPE=Release \
+        -DCOMPILER_RT_STACK_UNWIND=ON \
         -G "Unix Makefiles" \
         -DCMAKE_C_COMPILER="/usr/lib/ccache/gcc" \
         -DCMAKE_CXX_COMPILER="/usr/lib/ccache/g++" \
