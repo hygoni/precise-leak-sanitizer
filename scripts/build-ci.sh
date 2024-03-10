ab63991004bc13f85eab4c7466f877ca3bd48773
Hyeonggon Yoo <42.hyeyoo@gmail.com>
2024-03-10 14:13:53 +0900

    workflow: Add ccache for faster build during CI
    Add ccache save/restore which caches built objects and makes it faster
    to re-build later. To use ccache compiler for CI, create
    scripts/build-ci.sh which is a build script for CI only.
    
    Also, do not install clang 17, which is unnecessary for building llvm.
    
    Signed-off-by: Hyeonggon Yoo <42.hyeyoo@gmail.com>

---
+++ scripts/build-ci.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 1. Make build folder
+mkdir build
+cd build
+
+# 2. Build
+cmake -DLLVM_ENABLE_PROJECTS="llvm;clang;compiler-rt" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -G "Unix Makefiles" \
+        -DCMAKE_C_COMPILER="/usr/lib/ccache/gcc" \
+        -DCMAKE_CXX_COMPILER="/usr/lib/ccache/g++" \
+        ../llvm
+make -j$(nproc)
